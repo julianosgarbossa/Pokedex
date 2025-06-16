02b8faa1eb36efbd30e4086c2adebcc59165f6db
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-06-16 11:19:51 -0300

    Configurações Iniciais
    Criando projeto.
    
    Definindo nome, target, orientação...
    
    Configurando projeto para trabalhar com viewcode.

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Juliano Sgarbossa on 16/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
