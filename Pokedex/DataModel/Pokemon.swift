000fe04fe4bec2d1a57bfffac6e140365872dee3
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-06-16 15:56:17 -0300

    Model Mapping Pokemon
    Pegando a resposta da API PokemonLIstResponse e criando um novo modelo melhorado Pokemon, ideal para a tela de ListPokemons que vou criar posteriormente.

---
+++ Pokedex/DataModel/Pokemon.swift
@@ -0,0 +1,15 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Juliano Sgarbossa on 16/06/25.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let number: Int
+    let pokemonImage: String
+    let pokemonUrl: URL?
+}
--- Pokedex/Service/PokemonService.swift
+++ Pokedex/Service/PokemonService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol PokemonServiceProtocol {
-    func fetchPokemonList(completion: @escaping (Result<[PokemonResponse], Error>) -> Void)
+    func fetchPokemonList(completion: @escaping (Result<[Pokemon], Error>) -> Void)
 }
 
 final class PokemonService: PokemonServiceProtocol {
@@ -18,15 +18,16 @@ final class PokemonService: PokemonServiceProtocol {
         self.networkClient = networkClient
     }
     
-    func fetchPokemonList(completion: @escaping (Result<[PokemonResponse], any Error>) -> Void) {
+    func fetchPokemonList(completion: @escaping (Result<[Pokemon], any Error>) -> Void) {
         let urlString = "https://pokeapi.co/api/v2/pokemon?limit=151"
         
         networkClient.fetch(from: urlString, decodeTo: PokemonListResponse.self) { result in
             switch result {
             case .success(let response):
-                print(response)
+                let pokemons = response.results.map { $0.toDomainModel() }
+                completion(.success(pokemons))
             case .failure(let error):
-                print(error)
+                completion(.failure(error))
             }
         }
     }
